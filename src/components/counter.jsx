d5cb0e43d973480ba4581a4fc55f50f8294ddd0c
sman <sujanmanpr@gmail.com>
2020-02-23 12:03:33 -0800

    pass parameter in function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -37,8 +37,8 @@ class Counter extends Component {
             
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick = { () =>{
-        this.handleIncrement(product);
-    }} className="btn btn-secondary btn-sm">Increment</button>
+                this.handleIncrement(product);
+            }} className="btn btn-secondary btn-sm">Increment</button>
             {this.state.tags.length === 0 && "Please create a new tag!"}
             {this.renderTags()}
             </div>  );
