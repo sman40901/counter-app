e6010e0e85251c6515e8fb16597138451a276184
sman <sujanmanpr@gmail.com>
2020-03-01 13:39:07 -0800

    stateless function componet and before object destructring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counters from './components/counters';
+import React, { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Counters/>
-      </header>
-      
-    </div>
-  );
+class App extends Component {
+  // passing counters to the app which is the root
+
+  state = {
+    totalCount: 0,
+    imageUrl: "https://picsum.photos/200",
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    //instead of updating the array ourself we create new array and give it to react
+    // it does the updating
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters /* key */: counters /*value*/ });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+    // no single source of truth right now
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    /*
+    const c1 = this.state.counters.filter(c => c.id == counter.id);
+    console.log(counters);
+    c1.value ++;
+    */
+    /*
+   we are creating a clone of a counter because in react, 
+   manipulating object states is a big no no re
+   */
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters = {this.state.counters.length}/>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,63 +2,23 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-    state = {
-        totalCount: 0,
-        imageUrl: "https://picsum.photos/200",
-        counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-    };
-
-    handleDelete = counterId => {
-        //instead of updating the array ourself we create new array and give it to react
-        // it does the updating
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters/* key */: counters /*value*/ })
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(
-             c=>{
-                 c.value=0;
-                 return c;
-             }
-         );
-         this.setState({counters}) ;
-         // no single source of truth right now
-    };
-
-    handleIncrement = counter => {
-        const counters = [...this.state.counters];
-        /*
-        const c1 = this.state.counters.filter(c => c.id == counter.id);
-        console.log(counters);
-        c1.value ++;
-        */
-       const index = counters.indexOf(counter);
-       counters[index] = {...counter};
-       counters[index].value++;
-       this.setState({counters});
-    };
+   
 
     render() {
         return (
             <div>
                  <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-bg"
         >
           Reset
         </button>
-                {this.state.counters.map(counter => (
+                {this.props.counters.map(counter => (
                     <Counter
                         key={counter.id}
                         // key attrib internally by react
-                        onDelete={this.handleDelete}
-                        onIncrement = {this.handleIncrement}
+                        onDelete={this.props.onDelete}
+                        onIncrement = {this.props.onIncrement}
                         /*
                         value={counter.value}
                         id={counter.id}
