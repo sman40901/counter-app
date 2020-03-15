4f9fb0db7e43069da2f016ad45b3ff7153eb8c81
sman <sujanmanpr@gmail.com>
2020-03-15 13:03:58 -0700

    nav bar added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+/* stateless function component
+const NavBar = (props) => {
+    return (
+
+        <nav className="navbar navbar-light bg-light">
+
+            <a class="navbar-brand mb-0 h1">
+                Navbar &nbsp;
+                <span className='badge badge-pill badge-secondary'>
+                    {props.totalCounters}
+                </span>
+              </a>
+        </nav>
+    );
+}
+*/
+
+class NavBar extends Component {
+
+    render() {
+        console.log('navbar - rendered');
+        // object destructure example
+        const { totalCounters,totalValue } = this.props;
+        return (
+
+            <nav className="navbar navbar-light bg-light">
+
+                <a className="navbar-brand mb-0 h1">
+                    Navbar &nbsp;
+                    <span className='badge badge-pill badge-secondary'>
+                        {totalCounters}
+                    </span>
+                    &nbsp;
+                    <span className='badge badge-pill badge-secondary'>
+                        {totalValue}
+                    </span>
+                </a>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
